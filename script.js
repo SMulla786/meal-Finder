ba73505168db2d8eba35e4f3e315c93f2c09f205
SMulla786 <103066052+SMulla786@users.noreply.github.com>
2022-12-14 22:24:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ searchInput.addEventListener("keypress", (event) => {
     }
     if (searchInput.value == "Bapya") {
       document.getElementById("meals").innerHTML = `<img
-    src="https://photos.app.goo.gl/TxYPJrw9qGa2cG186" alt="" height="200px;"width="300px">`;
+    src="https://lh3.googleusercontent.com/GJy8nenghZXVPAgVz3_D5TCojue4gZuaxrJT9mM5xP5USd1P1uhbOcPjX8rGkAVHnAiW6BwNbPqEfiL4Nyn_r28IUWoiZHaUPiGmxQH5pJHTcRILkI5TYRWKi9GNOEYxFIC8SHGpThskZD0VISVdWxvskTFVf9HQgPBNcbTfCvOoAk0IIyu8GRhHkRNzTOZEXlJuQVtm5mhxVPmRtHMsEdxeNq9Ehd09R8tB8BVeEBG2N0rgX9iP0TjaMpMHeXSw5Dq735j8JQ97_yl5-ffW0JZbXfiehYB2vya3sVnCcVTmD1u-8_KhNm-YMRDfdwHq-ChXqKaJe2VPiKNIh0RrmAOk0Jx9HzKzV1HXLXdx5q4R1wSoPomnhFTfl9noVXlXECo382H5_AEAXDDDc-mYl35cZRIrQDajLF687v1MyXzQfSc04_psM7pxvPY9yHLHHXe98R5JpV23U8fXSIWwHGdif9b-EmEgamOTuAKC2wlot5ZNxMaFgEIdhusEhzXh7PRz2wlkfD0fbzMKhYr3jaar8thhb7wmIwnX1q6DmTGdEczyZkG8SIidjKuBYuKBPfCkRXHON1DnJuDCDTFhKnGofZrSqwceYCt91D8T_RVBn0Rl_uhzsrtV2w9zVFtrvIJPUNzCACdmEt5sEbeTa_tBzBTPE5pIVr9f0i8oipWBfzK4AQTwhiWGGxgTw8F5yjJdSuogxTsJTLKRBhXT1Y4_thx-FKDM9ze5XHKiLrvZ-RxelRIWynyJlLc5Dy-76L12usyBZgumztUt-6kbyxnPO2ZZBM7YI6cyg-b_8W3nSrg5PsrG99ui8zv-8DQ9G71Zxey8i7wCtMlfw7EigA3v38McS4s6R6HdU3-u2kqsZhbpVLaVPjXR6wazmU82rj8tYHdw47Zg8yU44mWZg04PuhF6KADhh7DlfNon6bEmzuYZXA=w890-h930-no?authuser=0" alt="" height="200px;"width="300px">`;
     }
     else {
       getMeal();
